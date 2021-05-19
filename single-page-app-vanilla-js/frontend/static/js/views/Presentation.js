00f7f444a6122a1f8b06245944cc74f5424bfc44
SergiuDev <sergiu.nistor@info.uaic.ro>
2021-05-19 17:14:44 +0300

    Nitpick

--- single-page-app-vanilla-js/frontend/static/js/views/Presentation.js
+++ single-page-app-vanilla-js/frontend/static/js/views/Presentation.js
@@ -9,7 +9,7 @@ export default class extends AbstractView {
     async getHtml() {
         return `
             <h1>Presentation</h1>
-            <embed src="static/resources/paper.pdf" width="100%" height="800vh" type="application/pdf" style="border:10px solid black">
-        `;
+            <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQFlPak9LEfct6UOhM8n842CNCEDK7aX0SmShyJUcvjkvZ-I0jtop2A-mkd-szaTw/embed?start=false&loop=false&delayms=3000" frameborder="0"  width="100%" height="800vh" type="application/pdf" style="border:10px solid black" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+		`;
     }
 }
